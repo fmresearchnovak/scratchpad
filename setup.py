bb3d6e40f5d7861234999fa8dded597ae4fe2ce8
Ed <ejnovak@email.wm.edu>
2019-07-19 22:31:10 -0400

    Packaging for pypi.org  See the project here: https://pypi.org/project/scratchpad-deadmund/

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_desc = fh.read()
+
+setuptools.setup(
+	name = "scratchpad-deadmund",
+	version = "1.0",
+	author = "Ed Novak",
+	author_email="enovak@fandm.edu",
+	description = "Notepad with basic math functions",
+	long_description = long_desc,
+	long_description_content_type="text/markdown",
+	url="https://github.com/fmresearchnovak/scratchpad",
+	packages=setuptools.find_packages(),
+	classifiers=["Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
